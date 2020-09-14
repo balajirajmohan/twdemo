217731972a1094dc085d5e72e04b69f2f269113a
balajirajmohanbr@gmail.com <balajirajmohanbr@gmail.com>
2020-09-14 23:28:27 +0530

    added jenkins file

--- Terraform/Resources/Jenkins Server/user_data.sh
+++ Terraform/Resources/Jenkins Server/user_data.sh
@@ -6,9 +6,14 @@ rpm --import http://pkg.jenkins.io/redhat/jenkins.io.key
 yum install jenkins -y
 yum install java -y
 systemctl start jenkins
+sudo yum
 chkconfig jenkins on
 aws s3 cp s3://pemkeytw/mediawiki.pem /root
 sudo ssh-keygen
+sudo yum install -y yum install http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.107-1.el7_6.noarch.rpm -y
+sudo yum install docker-ce -y
+
+
 
 
 
