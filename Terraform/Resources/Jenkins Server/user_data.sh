19575da02d7120e3799e194aef7419e911dc5b71
balajirajmohanbr@gmail.com <balajirajmohanbr@gmail.com>
2020-09-15 11:12:54 +0530

    removed rpm's

--- Terraform/Resources/Jenkins Server/user_data.sh
+++ Terraform/Resources/Jenkins Server/user_data.sh
@@ -15,7 +15,8 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo chmod 666 /var/run/docker.sock
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install ansible -y
+
+
 
 
 
