08dc173b18ecc8ffc211c47453a9e002d78b4313
balajirajmohanbr@gmail.com <balajirajmohanbr@gmail.com>
2020-09-18 14:17:54 +0530

    adding updated Jenkins user data

--- Terraform/Resources/Jenkins Server/user_data.sh
+++ Terraform/Resources/Jenkins Server/user_data.sh
@@ -12,7 +12,7 @@ yum  -y remove  docker-common docker container-selinux docker-selinux docker-eng
 yum -y install lvm2 device-mapper device-mapper-persistent-data device-mapper-event device-mapper-libs device-mapper-event-libs
 yum -y install  wget
 wget https://download.docker.com/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
-sudo yum install -y yum install http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.107-1.el7_6.noarch.rpm
+sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.107-1.el7_6.noarch.rpm
 yum -y install docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
